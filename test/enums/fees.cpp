6290ed00651b9cf83fbdac46854f25954c337fce
Chris <chrisjohnsonmail@gmail.com>
2018-12-21 21:51:03 -0600

    chore:  Update to use 64 bit operations
    Updated to use newly added 64 bit integer operations in AUnit and now align with GTest.

--- test/enums/fees.cpp
+++ test/enums/fees.cpp
@@ -7,29 +7,29 @@ TEST(enums, fees)
     Ark::Crypto::Enums::Fees fees;
     
     auto feeZERO = fees.TRANSFER;
-    ASSERT_TRUE(10000000 == feeZERO);
+    ASSERT_EQ(10000000ull, feeZERO);
 
     auto feeONE = fees.SECOND_SIGNATURE_REGISTRATION;
-    ASSERT_TRUE(500000000 == feeONE);
+    ASSERT_EQ(500000000ull, feeONE);
 
     auto feeTWO = fees.DELEGATE_REGISTRATION;
-    ASSERT_TRUE(2500000000 == feeTWO);
+    ASSERT_EQ(2500000000ull, feeTWO);
 
     auto feeTHREE = fees.VOTE;
-    ASSERT_TRUE(100000000 == feeTHREE);
+    ASSERT_EQ(100000000ull, feeTHREE);
 
     auto feeFOUR = fees.MULTI_SIGNATURE_REGISTRATION;
-    ASSERT_TRUE(500000000 == feeFOUR);
+    ASSERT_EQ(500000000ull, feeFOUR);
 
     auto feeFIVE = fees.IPFS;
-    ASSERT_TRUE(0 == feeFIVE);
+    ASSERT_EQ(0ull, feeFIVE);
 
     auto feeSIX = fees.TIMELOCK_TRANSFER;
-    ASSERT_TRUE(0 == feeSIX);
+    ASSERT_EQ(0ull, feeSIX);
 
     auto feeSEVEN = fees.MULTI_PAYMENT;
-    ASSERT_TRUE(0 == feeSEVEN);
+    ASSERT_EQ(0ull, feeSEVEN);
 
     auto feeEIGHT = fees.DELEGATE_RESIGNATION;
-    ASSERT_TRUE(0 == feeEIGHT);
+    ASSERT_EQ(0ull, feeEIGHT);
 }
--- test/utils/slot.cpp
+++ test/utils/slot.cpp
@@ -6,37 +6,16 @@
 
 TEST(utilities, slots_time) {
   const auto devnet = Ark::Crypto::Networks::Devnet;
-  
-#ifdef USE_IOT
-  // IoT does not support 64 bit in unit tests yet
-  //ASSERT_EQ(10ull, ARK::Utilities::slots::time(1490101210000, devnet));
-  ASSERT_TRUE(Ark::Crypto::Utils::Slot::time(1490101210000, devnet) == 10ull);
-#else
   ASSERT_EQ(10ull, Ark::Crypto::Utils::Slot::time(1490101210000, devnet));
-#endif
 }
 
 TEST(utilities, slots_begin_epoch_time) {
   const auto devnet = Ark::Crypto::Networks::Devnet;
-
-#ifdef USE_IOT
-  // IoT does not support 64 bit in unit tests yet
-  //ASSERT_EQ(1490101200ull, ARK::Utilities::slots::begin_epoch_time(devnet));
-  ASSERT_TRUE(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::milliseconds(Ark::Crypto::Utils::Slot::begin_epoch_time(devnet))).count() == 1490101200ull);
-#else
   ASSERT_EQ(1490101200ull, std::chrono::duration_cast<std::chrono::seconds>(std::chrono::milliseconds(Ark::Crypto::Utils::Slot::begin_epoch_time(devnet))).count());
-#endif
 }
 
 TEST(utilities, slots_get_real_time) {
   const auto devnet = Ark::Crypto::Networks::Devnet;
-
-#ifdef USE_IOT
-  // IoT does not support 64 bit in unit tests yet
-  //ASSERT_EQ(1490101210000ull, ARK::Utilities::slots::get_real_time(10));
-  ASSERT_TRUE(Ark::Crypto::Utils::Slot::real_time(10, devnet) == 1490101210000ull);
-#else
   ASSERT_EQ(1490101210000ull, Ark::Crypto::Utils::Slot::real_time(10, devnet));
-#endif
 }
 
