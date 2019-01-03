94df84c549c4ed3a3c0a7d63291859bf0e7deddc
Chris Johnson <chrisjohnsonmail@gmail.com>
2019-01-03 11:08:58 -0600

    chore:  Print warnings in CI

--- .circleci/clang_tidy.sh
+++ .circleci/clang_tidy.sh
@@ -9,5 +9,8 @@ if [[ -n $(grep "error: " output.txt) ]]; then
     grep --color -E '^|warning: |error: ' output.txt
     exit -1;
 else
+    # still output the file to show warnings
+    echo ""
+    grep --color -E '^|warning: |error: ' output.txt
     echo -e "\033[1;32m\xE2\x9C\x93 passed:\033[0m $1";
 fi
