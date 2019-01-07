9ccd1567ad473468c21537647e2001a2ddb526d1
Chris <chrisjohnsonmail@gmail.com>
2019-01-07 12:44:52 -0600

    chore:  Added ignore directories for clang format

--- .circleci/clang_format.sh
+++ .circleci/clang_format.sh
@@ -16,7 +16,7 @@ $CLANG_FORMAT --version
 #    files_to_lint="$(git diff --name-only --diff-filter=ACMRTUXB $TRAVIS_COMMIT_RANGE | grep '^src/[^.]*[.]\(cpp\|h\)$' || true)"
 #else
     # Check everything for branch pushes
-    files_to_lint="$(find src/ -name '*.cpp' -or -name '*.h')"
+    files_to_lint="$(find . -type f -not -path "*lib*" -not -path "*src/lib*" -path "*src/lib*" -not -path "*test/lib*" -name '*.cpp' -or -name '*.h')"
 #fi
 
 # Turn off tracing for this because it's too verbose
