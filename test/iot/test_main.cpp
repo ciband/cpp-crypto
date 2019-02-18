cd2f35e4efbd4323aa0365f652ce620d95159243
Chris Johnson <chrisjohnsonmail@gmail.com>
2019-02-18 09:42:58 -0600

    refactor: Convert to use GTest

--- test/iot/test_main.cpp
+++ test/iot/test_main.cpp
@@ -1,6 +1,8 @@
 #if (defined PLATFORMIO && defined UNIT_TEST)
 
-#include <AUnit.h>
+#include "gtest/gtest.h"
+
+#include <Arduino.h>
 
 void setup() {
     Serial.begin(115200);
@@ -8,11 +10,15 @@ void setup() {
 	while (!Serial) { delay(100); };
 	// ^for the Arduino Leonardo/Micro only
 	
-	aunit::TestRunner::setTimeout(0);
-	
+	testing::InitGoogleTest();
 	delay(1000);
+
+  RUN_ALL_TESTS();
 }
 
-void loop() { aunit::TestRunner::run(); }
+void loop() {
+  // do nothing
+  delay(1000);
+}
 
 #endif
